67111694219b200c2eeab69ebdc6f548f6c31373
Rad <kalmanjudin2000@gmail.com>
2024-12-27 06:32:58 +0000

    Added mongoDB

---
+++ mongo-init.js
@@ -0,0 +1,23 @@
+// Switch to the database that should be created
+db = db.getSiblingDB("coventryCityDB");
+
+// Create the user with readWrite access to the "coventryCityDB"
+db.createUser({
+    user: "dbuser",
+    pwd: "dbpassword",
+    roles: [
+        {
+            role: "readWrite",
+            db: "coventryCityDB",
+        },
+    ],
+});
+
+// Create a collection to initialize the database
+db.createCollection("football_matches");
+
+// Insert some sample data to ensure the database is initialized
+// db.testCollection.insertMany([
+//     { name: "Test Data 1", value: 123 },
+//     { name: "Test Data 2", value: 456 },
+// ]);
